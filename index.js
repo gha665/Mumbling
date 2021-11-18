115c3247073b3483293e80c9f5fcbe7d9f3b4bb0
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2021-11-18 03:38:45 -0800

    comments on algorithm

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function accum(s) {
-  
   let totalString = "";
   let characters = s.split("");
-  
+  //splits characters
   for (i = 0; i < characters.length; i++) {
-    let characterSubstring = "";
-    for (j = 0; j < (i + 1); j++) {
+    let characterSubstring = ""; //2 for loops for comparison
+    for (j = 0; j < i + 1; j++) {
       let letterToAdd = "";
       if (j === 0) {
         letterToAdd = characters[i].toUpperCase();
@@ -16,7 +15,7 @@ function accum(s) {
     }
     totalString += characterSubstring + "-";
   }
-  
-  totalString = totalString.slice(0, -1); 
+
+  totalString = totalString.slice(0, -1); //slice index to start end is index before which to end extraction
   return totalString;
 }
