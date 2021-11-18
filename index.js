5caa02f1e1d7d35986ccc92951eb4288ceb4791c
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2021-11-18 03:52:23 -0800

    working less complex solution

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
+//working solution for codewars
 function accum(s) {
-  let totalString = "";
-  let characters = s.split("");
-  //splits characters
-  for (i = 0; i < characters.length; i++) {
-    let characterSubstring = ""; //2 for loops for comparison
-    for (j = 0; j < i + 1; j++) {
-      let letterToAdd = "";
-      if (j === 0) {
-        letterToAdd = characters[i].toUpperCase();
-      } else {
-        letterToAdd = characters[i].toLowerCase();
-      }
-      characterSubstring += letterToAdd;
-    }
-    totalString += characterSubstring + "-";
+  var letters = s.split(""),
+    words = [];
+
+  for (var i = 0; i < letters.length; i++) {
+    words.push(
+      letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase())
+    );
   }
 
-  totalString = totalString.slice(0, -1); //slice index to start end is index before which to end extraction
-  return totalString;
+  return words.join("-");
 }
+
+// function accum(s) {
+//   let totalString = "";
+//   let characters = s.split("");
+//   //splits characters
+//   for (i = 0; i < characters.length; i++) {
+//     let characterSubstring = ""; //2 for loops for comparison
+//     for (j = 0; j < i + 1; j++) {
+//       let letterToAdd = "";
+//       if (j === 0) {
+//         letterToAdd = characters[i].toUpperCase();
+//       } else {
+//         letterToAdd = characters[i].toLowerCase();
+//       }
+//       characterSubstring += letterToAdd;
+//     }
+//     totalString += characterSubstring + "-";
+//   }
+
+//   totalString = totalString.slice(0, -1); //slice index to start end is index before which to end extraction
+//   return totalString;
+// }
